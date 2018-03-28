8afc8924becdf2ce23e3f75e8db7010d82df62db
Usman Adeel <adeel@citypolyhs.org>
2018-03-28 22:26:23 +0000

    I added the first part of my gif finder project from popcode

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
+/*global_*/
 $(document).ready(function(){
-  
+  function giphyURLWithSearchTerm(searchTerm) {
+    // write a function that will return a url for the giphy API with
+    // the searchTerm provided in the parameters
+   var newUrl = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+	
+    return newUrl;
+
+ 
+}
+
+function appendImageToBody(srcURL) {
+    // write a function that will append an <img> to the body with the
+    // URL provided in the parameters
+    $('body').append('<img src=' + srcURL + '>');
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    $.ajax({
+      url: giphyURLWithSearchTerm(searchTerm),
+      method: "GET",
+      
+      success: function(response) {
+           var url = response.data[0].images.original.url;
+           appendImageToBody(url);
+           
+      },
+    }); 
+}
+
+//Add a click handler beloe to call the function when the button is clicked
+$("button").click(function() {
+var searchTerm = $("input").val();
+callGiphyAPIWithSearchTerm(searchTerm);
+});
+    
   
   
   
